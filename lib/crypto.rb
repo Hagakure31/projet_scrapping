050836c219465456aef12521d2c2930e61c3b58c
Hagakure31 <luffyeic@gmail.com>
2019-01-17 18:40:53 +0100

    ajout des fichiers

---
+++ lib/crypto.rb
@@ -0,0 +1,43 @@
+require 'nokogiri'
+require 'rubygems'
+require 'open-uri'
+
+def crypto_scrapper
+
+  value = []
+  currency = []
+
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+
+  page.xpath('//a[@class = "link-secondary"]').each do |devise|
+
+    currency << devise.text
+
+end
+
+# print currency
+
+
+  page.xpath('//a[@class = "price"]').each do |valeur|
+
+    value << valeur.text
+
+end
+
+# print value
+
+  values = value.map{|e| e.delete('$').to_f }
+
+  my_hash = Hash[currency.zip(values.map)]
+
+
+  a = []
+
+  my_hash.each {|index| a << {index[0] => index[1]}}
+
+  puts a
+
+end
+
+crypto_scrapper
---
+++ lib/townhall_email.rb
@@ -0,0 +1,43 @@
+require 'nokogiri'
+require 'rubygems'
+require 'open-uri'
+
+townhall=[]
+
+page = Nokogiri::HTML(open("http://www.annuaire-des-mairies.com/val-d-oise.html"))
+
+page.xpath('//p/a').each do |name|
+  townhall << name.text
+end
+
+
+
+
+
+
+
+email = []
+
+page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+
+news_links = page.css("a")
+
+news_links.each do |link|
+
+    page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
+
+    page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |mail|
+
+    email << mail.text
+
+  end
+end
+
+
+hash = Hash[townhall.zip(email)]
+
+a = []
+
+hash.each {|index| a << {index[0] => index[1]}}
+
+puts a
